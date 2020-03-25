37c1f9c9c6939ff17540b2c9a44efd70b749c5ef
wangbei98 <beiwang121@163.com>
2020-03-25 18:06:42 +0800

    实现图片缩略图

--- backend/APIS/resources.py
+++ backend/APIS/resources.py
@@ -15,6 +15,8 @@ from extensions import db,login_manager
 from werkzeug.datastructures import FileStorage
 from settings import config
 from flask import send_file,make_response,send_from_directory,stream_with_context
+from PIL import Image
+import io
 
 UPLOAD_FOLDER = config['UPLOAD_FOLDER']
 CHUNK_SIZE = config['CHUNK_SIZE']
@@ -333,9 +335,13 @@ class PreviewAPI(Resource):
 	def get(self):
 		parse = reqparse.RequestParser()
 		parse.add_argument('id',type=int,help='错误的id',default='0')
+		parse.add_argument('width',type=int,help='wrong width',default=300)
+		parse.add_argument('height',type=int,help='wrong height',default=300)
 		args = parse.parse_args()
 		# 获取当前文件夹id
 		file_id = args.get('id')
+		width = args.get('width')
+		height = args.get('height')
 		try:
 			file_node = FileNode.query.get(file_id)
 		except:
@@ -351,8 +357,14 @@ class PreviewAPI(Resource):
 				# 结合 UPLOAD_FOLDER 得到最终文件的存储路径
 			target_file = os.path.join(os.path.expanduser(UPLOAD_FOLDER), actual_filename)
 			if os.path.exists(target_file):
-				img_data = open(target_file,"rb").read()
-				response = make_response(img_data)
+				img_data = Image.open(target_file)
+				img_data.thumbnail((width,height))
+
+				fp = io.BytesIO()
+				format = Image.registered_extensions()['.'+node_type]
+				img_data.save(fp, format)
+
+				response = make_response(fp.getvalue())
 				response.headers['Content-Type'] = 'image/' + node_type
 				return response
 			else:
--- backend/settings.py
+++ backend/settings.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 config={
 	'UPLOAD_FOLDER':os.path.join(basedir,'backend/upload'),
-	'CHUNK_SIZE':1024
+	'CHUNK_SIZE':1024,
+	'PHOTO_TYPES':['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF']
 }
